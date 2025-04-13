77fd83e4338ada16ae0c2fd00a33652b88bc9719
divyanshu40 <divyanshuprajapati40@gmail.com>
2025-04-13 18:42:48 +0530

    index.js file updated

--- index.js
+++ index.js
@@ -41,6 +41,28 @@ async function getEventById(eventId) {
     return { event: eventDetails };
 }
 
+// function to get events by title
+async function getEventByTitle(eventTitle) {
+    const eventDetails = await event.findOne({ title: eventTitle });
+    if (! eventDetails) {
+        return null;
+    }
+    return { event: eventDetails };
+}
+
+// function to find events by tag
+async function getEventsByTag(tag) {
+    const events = await event.find();
+    const filteredEvents = events.filter(eventData => eventData.eventTags.includes(tag) === true);
+    return { events: filteredEvents };
+}
+
+// function to get events by type
+async function getEventsByType(type) {
+    const events = await event.find({ eventType: type });
+    return { events: events };
+}
+
 // api to get all events
 app.get("/events", async (req, res) => {
     try {
@@ -61,6 +83,48 @@ app.get("/events/details/:id", async (req, res) => {
         const response = await getEventById(eventId);
         if (response === null) {
             return res.status(404).json({ message: "Event not found" });
+        }
+        return res.status(200).json(response);
+    } catch(error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// api to get event by title
+app.get("/events/title/:title", async (req, res) => {
+    const eventTitle = req.params.title;
+    try {
+        const response = await getEventByTitle(eventTitle);
+        if (response === null) {
+            return res.status(404).json({ message: "Event not found" });
+        }
+        return res.status(200).json(response);
+    } catch(error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// api to get events by tag
+app.get("/events/tag/:tag", async (req, res) => {
+    const tag = req.params.tag;
+    try {
+        const response = await getEventsByTag(tag);
+        if (response.events.length === 0) {
+            return res.status(404).json({ message: "No events found"});
+        }
+        return res.status(200).json(response);
+    } catch(error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// api to get events by type
+app.get("/events/event-type/:type", async (req, res) => {
+    const type = req.params.type;
+    try {
+        const response = await getEventsByType(type);
+        if (response.events.length === 0) {
+            return res.status(404).json({ message: "Events not found" });
         }
         return res.status(200).json(response);
     } catch(error) {
